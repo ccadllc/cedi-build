05f7c885fb342151341ea6321b9a1416ec4b24fd
sbuzzard <steve_buzzard@cable.comcast.com>
2018-08-22 16:23:05 -0400

    bump up the versions of 2.12 and 2.11 to the latest - can't build with 2.13 yet as some cedi deps don't have published targets for it.

--- src/main/scala/com/ccadllc/cedi/build/BuildSettings.scala
+++ src/main/scala/com/ccadllc/cedi/build/BuildSettings.scala
@@ -82,7 +82,7 @@ object BuildSettings extends AutoPlugin {
 
   private def scalaSettings = Seq(
     scalaVersion := crossScalaVersions.value.head,
-    crossScalaVersions := Seq("2.12.2", "2.11.11"),
+    crossScalaVersions := Seq("2.12.6", "2.11.12"),
     scalacOptions ++= Seq(
       "-deprecation",
       "-encoding", "UTF-8",
